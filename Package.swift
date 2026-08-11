6f20fd833952cbb2dda74059b511b430cc71ea76
Brandon <btoms.20@gmail.com>
2026-08-11 13:59:51 +0200

    bumped swift-libp2p to 0.3.5 to capture LibP2PTestUtils

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMinor(from: "0.3.5")),
 
         // Test Dependencies
         .package(url: "https://github.com/swift-libp2p/swift-libp2p-yamux.git", .upToNextMinor(from: "0.2.0")),
